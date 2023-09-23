5db1e8927f3c243991ba32f882b10c1e56ad3167
kush@19 <55134699+kushagra19@users.noreply.github.com>
2023-09-23 18:01:19 -0400

    Added new lab2.c

---
+++ Lab2/lab2_1.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+void read_file_name(char *file_name, int mode)
+{	// mode = 0 is input, mode = 1 is output
+	if(mode == 0)
+		printf("Enter input filename: ");
+	else if(mode == 1)
+		printf("Enter output filename: ");
+	else
+		printf("Invalid Mode.");
+	scanf("%s", file_name);
+
+	printf("Entered filename: %s\n", file_name);
+}
+
+int routine(char *input_file, char *output_file)
+{
+	FILE *input_file_ptr, *output_file_ptr;
+	char buffer[512];	//buffer to store data from input file
+	
+	//open file
+	input_file_ptr = fopen(input_file, "r");
+
+	//check of file opened withour error
+	
+	if(input_file_ptr == NULL)
+	{
+		perror("ERROR");
+		fclose(input_file_ptr);
+		return 0;
+	}
+
+	//open file
+	
+	output_file_ptr = fopen(output_file, "wx");
+	
+	//check if file already exists
+	if(output_file_ptr == NULL)
+	{
+		perror("ERROR");
+		fclose(output_file_ptr);
+		return 0;
+	}
+	
+	//Reading contents of file line by line
+
+	while(fgets(buffer, sizeof(buffer), input_file_ptr))
+	{
+		fputs(buffer, output_file_ptr);
+	}
+	
+	printf("\nContents of %s transferred successfully to %s.\n", input_file, output_file);
+
+	fclose(output_file_ptr);
+	fclose(input_file_ptr);
+}
+
+
+int main()
+{
+	char input_file_name[100];	//maximum characters in filename
+	//read input file
+	read_file_name(input_file_name, 0);
+        
+	char output_file_name[100];	//maximum characters in filename
+	//read output file
+	read_file_name(output_file_name, 1);
+	
+	//call routine
+	int test = routine(input_file_name, output_file_name);
+
+	return 0;
+}
